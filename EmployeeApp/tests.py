d00f0be8f1114e2cca092351fd165fa75f3d0136
Plamen Gunchev <plamenmgunchev@gmail.com>
2022-12-16 22:57:38 +0200

    Tests: CompanyView test

--- EmployeeApp/tests.py
+++ EmployeeApp/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase, APIRequestFactory
+from .views import CompaniesList, CompanyCreate
+from django.urls import reverse
 
-# Create your tests here.
+
+class CompanyListCreateTestCase(APITestCase):
+    
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.view = CompaniesList.as_view()
+        self.url = reverse("companies_view")
+        
+
+    def test_list_companies(self):
+        
+        request = self.factory.get(self.url)
+        response = self.view(request)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_company_creation(self):
+        pass
