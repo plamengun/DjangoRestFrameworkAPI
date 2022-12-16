41465d9cc2184926b37d12fa381c064bc811886d
Plamen Gunchev <plamenmgunchev@gmail.com>
2022-12-16 16:00:51 +0200

    Added Interactive documentation

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # DjangoRestFrameworkAPI
+
+### Access to Swagger Documentation at: http://127.0.0.1:8000/ with running local server
--- project_files/settings.py
+++ project_files/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'EmployeeApp.apps.EmployeeappConfig',
     'accounts',
+    'drf_yasg',
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
