a08efb5d6df3d3c259e6346e3d588a55f7fdd548
Plamen Gunchev <plamenmgunchev@gmail.com>
2022-12-18 12:25:56 +0200

    Pytest accounts tested, GET left

--- accounts/views.py
+++ accounts/views.py
@@ -9,45 +9,46 @@ from rest_framework.views import APIView
 class SignUpView(generics.GenericAPIView):
     serializer_class = SignUpSerializer
 
-    def post(self, request:Request):
+    def post(self, request: Request):
         data = request.data
 
-        serializer=self.serializer_class(data=data)
+        serializer = self.serializer_class(data=data)
 
         if serializer.is_valid():
             serializer.save()
 
-            response={
+            response = {
                 "message": "User Created Successfully",
                 "data": serializer.data
             }
 
             return Response(data=response, status=status.HTTP_201_CREATED)
-        
+
         return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class LoginView(APIView):
 
-    def post(self, request:Request):
-        email=request.data.get('email')
-        password=request.data.get('password')
+    def post(self, request: Request):
+        email = request.data.get('email')
+        password = request.data.get('password')
 
         user = authenticate(email=email, password=password)
 
         if user:
-            response={
+            response = {
                 "message": "Login Successful",
                 "token": user.auth_token.key
             }
             return Response(data=response, status=status.HTTP_200_OK)
         else:
-            return Response(data={"message":"Invalid username or password"})
+            return Response(data={"message": "Invalid username or password"},
+                            status=status.HTTP_401_UNAUTHORIZED)
 
-    def get(self, request:Request):
-        content={
-            "user":str(request.user),
-            "auth":str(request.auth)
+    def get(self, request: Request):
+        content = {
+            "user": str(request.user),
+            "auth": str(request.auth)
         }
 
         return Response(data=content, status=status.HTTP_200_OK)
--- tests/accounts/test_accounts.py
+++ tests/accounts/test_accounts.py
@@ -1,17 +1,85 @@
 import pytest
 from rest_framework.test import APIClient
+from rest_framework.serializers import ErrorDetail
 
 client = APIClient()
+MOCK_USER = {'email': 'plamenmgunchev@gmail.com', 'username': 'Plamen'}
 
 
 @pytest.mark.django_db
-def test_signup_view():
+def test_signup_view_post_when_successful():
     payload = {
         "username": "Plamen",
         "email": "plamenmgunchev@gmail.com",
         "password": "12345678"
     }
 
-    response = client.post("/api/auth/signup/", payload)
+    response = client.post("/api/auth/signup", payload)
 
     assert response.data["message"] == "User Created Successfully"
+    assert response.data["data"] == MOCK_USER
+
+
+@pytest.mark.django_db
+def test_signup_view_post_when_unsuccessful():
+    payload = {}
+
+    response = client.post("/api/auth/signup", payload)
+
+    assert response.data['email'] == [ErrorDetail(string='This field is required.',
+                                                  code='required')]
+    assert response.data['password'] == [ErrorDetail(string='This field is required.',
+                                                     code='required')]
+    assert response.data['username'] == [ErrorDetail(string='This field is required.',
+                                                     code='required')]
+
+
+@pytest.mark.django_db
+def test_login_view_post_when_successful():
+    payload = {
+        "username": "Plamen",
+        "email": "plamenmgunchev@gmail.com",
+        "password": "12345678"
+    }
+
+    client.post("/api/auth/signup", payload)
+
+    response = client.post("/api/auth/login", {"email": "plamenmgunchev@gmail.com",
+                                               "password": "12345678"})
+
+    assert response.data["message"] == "Login Successful"
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_login_view_post_when_unsuccessful():
+    payload = {
+        "username": "Plamen",
+        "email": "plamenmgunchev@gmail.com",
+        "password": "12345678"
+    }
+
+    client.post("/api/auth/signup", payload)
+
+    response = client.post("/api/auth/login", {"email": "plamenmgunchev@gmail.com",
+                                               "password": "1"})
+
+    assert response.data["message"] == "Invalid username or password"
+    assert response.status_code == 401
+
+
+# @pytest.mark.django_db
+# def test_get_logged_in_user_view():
+#     payload = {
+#         "username": "Plamen",
+#         "email": "plamenmgunchev@gmail.com",
+#         "password": "12345678"
+#     }
+#     client.post("/api/auth/signup", payload)
+#     request = client.post("/api/auth/login", {"email": "plamenmgunchev@gmail.com",
+#                                               "password": "12345678"})
+#
+#     response = client.get("/api/auth/login", data=request.data['token'])
+#
+#     assert response.data["user"] == 'Plamen'
+#     # assert response.status_code == 200
