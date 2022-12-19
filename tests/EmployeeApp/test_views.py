4df2bd19f17329b4cc26a58f261a07e1804fe2b8
Plamen Gunchev <plamenmgunchev@gmail.com>
2022-12-19 09:47:36 +0200

    database file upload

--- tests/EmployeeApp/test_views.py
+++ tests/EmployeeApp/test_views.py
@@ -1,19 +1,36 @@
 import pytest
-from rest_framework.test import APIClient
-
-
-client = APIClient()
+from rest_framework.test import APIRequestFactory, force_authenticate
+from accounts.models import User
+from EmployeeApp.views import CompanyCreate
 
 
 @pytest.mark.django_db
-def test_signup_view():
-    payload = {
-        "company_name": "comp13",
-        "company_description": "comp descr",
-        "company_logo": "logo.asd"
-    }
+def test_company_create():
+    # Create a client and log in as a user
+    factory = APIRequestFactory()
+    user = User.objects.create_user(username='testuser', email="test@gmail.com",
+                                    password='testpass')
+    get_user = User.objects.get(username='testuser')
+    view = CompanyCreate.as_view()
 
-    response = client.post("/api/companies", payload)
-    data = response.data
+    # Create a company
+    data = {'company_name': 'Test Company',
+            'company_description': 'comp descr',
+            'company_logo': 'logo.asd'
+            }
+    request = factory.post('/api/companies', data=data, format='json')
+    force_authenticate(request, user=get_user)
+    response = view(request)
+    assert User.objects.count() == 1
 
-    assert response.data["detail"] == payload
+    # # Check that the company was created in the database
+    # company = Companies.objects.get(company_name='Test Company')
+    # assert company.user_id == user.id
+    #
+    # # Try to create a company with the same name
+    # data = {'company_name': 'Test Company'}
+    # response = client.post('/api/companies', data=data, format='json')
+    # assert response.status_code == 400
+    #
+    # # Check that the company was not created in the database
+    # assert Companies.objects.filter(company_name='Test Company').count() == 1
